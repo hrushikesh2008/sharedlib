5eb49e20647d0bbce50ffd33b5b1736abb0d5c98
hrushikesh2008 <108468971+hrushikesh2008@users.noreply.github.com>
2025-01-12 17:11:08 +0530

    Update pkgwithskiptst.groovy

--- vars/pkgwithskiptst.groovy
+++ vars/pkgwithskiptst.groovy
@@ -1,4 +1,7 @@
 def call() {   
-     sh 'mvn package -DskipTests=true'      
+
+     def mavenCommand = 'mvn package -DskipTests=true'
+	
+	sh script: mavnCommand, returnStatus: true      
     
 }
